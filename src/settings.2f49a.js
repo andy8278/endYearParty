02fac85e35fca32dc6f2843695fce451cb6830e2
andy8278 <andy8278@yahoo.com.tw>
2025-01-06 17:53:58 +0800

    V1.0.2
     NoEnd

---
+++ src/settings.2f49a.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Reel.fire",orientation:"landscape",jsList:[],bundleVers:{resources:"760b7",internal:"d17e2",main:"369f7"}};
