f5c707ba2b64e60531a6ff369c52e8b04344e614
andy8278 <andy8278@yahoo.com.tw>
2025-01-09 13:38:18 +0800

    V1.2

---
+++ src/settings.75ab8.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Reel.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"52d36",resources:"34156",main:"6fdd3"}};
